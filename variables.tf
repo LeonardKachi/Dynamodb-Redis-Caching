ed0c7520314dd047c6e476d798e9a8572fa07951
Obidiegwu Onyedikachi Henry <henryleo480@gmail.com>
2025-04-15 15:26:01 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "redis_node_type" {
+  description = "ElastiCache Redis node type"
+  type        = string
+  default     = "cache.t3.micro"
+}
+
+variable "cache_ttl" {
+  description = "Redis cache TTL in seconds"
+  type        = number
+  default     = 300
+}
