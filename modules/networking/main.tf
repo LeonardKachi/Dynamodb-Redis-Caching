c5239b7aeaed9328c21a4138012ecd313ccf7cf1
Obidiegwu Onyedikachi Henry <henryleo480@gmail.com>
2025-04-15 15:27:22 +0100

    Create main.tf

---
+++ modules/networking/main.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.environment}-private-${count.index}"
+  }
+}
+
+resource "aws_security_group" "lambda" {
+  name        = "${var.environment}-lambda-sg"
+  description = "Security group for Lambda functions"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-lambda-sg"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
